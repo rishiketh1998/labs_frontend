9e30cb058e156c1d860da2528764ed45e7243bfb
rishi <prabhak8@uni.coventry.ac.uk>
2021-03-17 05:40:04 +0000

    set up react and installed required packages

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="display-2">Apption Labs</h1>
+    </div>
+  );
+}
+
+export default App;
