f367b8ee30f5b0c1fc23467f60bc529b278e659d
rishi <prabhak8@uni.coventry.ac.uk>
2021-03-17 06:14:00 +0000

    added a nvabar-component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Header } from './componenets/header.js'
 
 function App() {
   return (
     <div className="App">
-      <h1 className="display-2">Apption Labs</h1>
+      <Header />
     </div>
   );
 }
